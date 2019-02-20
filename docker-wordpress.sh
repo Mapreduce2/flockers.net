497205aee4904a5bc7a9be6d9c60ff1b4dcb848a
Blake <Blake.Russo@live.com>
2019-02-20 16:39:30 -0500

    Create docker-wordpress.sh
    docker-compose up -d

---
+++ docker-wordpress.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#script to build wordpress docker container with secondaru mariadb container. Requires docker engine. yum install -y docker*
+
+
+mkdir wordpress-site && cd wordpress-site
+
+touch docker-compose.yml
+
+cat > docker-compose.yml <<EOL
+version: "2"
+services:
+  my-wpdb:
+    image: mariadb
+    ports:
+      - "8081:3306"
+    environment:
+      MYSQL_ROOT_PASSWORD: ChangeMeIfYouWant
+  my-wp:
+    image: wordpress
+    volumes:
+      - ./:/var/www/html
+    ports:
+      - "8080:80"
+    links:
+      - my-wpdb:mysql
+    environment:
+      WORDPRESS_DB_PASSWORD: ChangeMeIfYouWant
+EOL
